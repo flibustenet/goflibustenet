6b3b4913ff2eea8d51a406ea187900dd454c3dca
Wilk <wilk@flibuste.net>
2022-08-15 11:19:22 +0200

    clean

--- main.go
+++ main.go
@@ -9,10 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		u := r.URL.Path[1:]
-		fmt.Fprintf(w, `
-<meta name="go-import" content="go.flibuste.net/%s git https://github.com/flibustenet/%s">
-Hello, you've requested: %s\n
-`, u, u, u)
+		fmt.Fprintf(w, `<meta name="go-import" content="go.flibuste.net/%s git https://github.com/flibustenet/%s">`, u, u, u)
 	})
 	fmt.Println("Listen on :" + os.Getenv("PORT"))
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
